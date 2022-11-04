d420c8c02f0919fedee62ef7c1585e05af8b9f21
oushkaroline <ana.freire2@estudante.ifms.edu.br>
2022-11-03 22:10:52 -0400

    Funcionalidade: Remover Tarefa

--- src/js/lista-tarefas.js
+++ src/js/lista-tarefas.js
@@ -12,6 +12,13 @@ btnAdicionar.addEventListener('click', function (){
     <div> ${descricaoTarefa} </div>
     <button class = "btn-excluir"> Excluir </button>
     `
+    li.addEventListener ('click', function (event) {
+        if (event.target.className === 'btn-excluir') {
+            listaTarefas.removeChild(li)
+
+        }
+
+    })
 
     listaTarefas.appendChild(li)
 })
