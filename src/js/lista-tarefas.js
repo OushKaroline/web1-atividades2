6a67e3c17c3b8446f9659665a4498eae133632d9
oushkaroline <ana.freire2@estudante.ifms.edu.br>
2022-11-03 21:59:58 -0400

    first commit

---
+++ src/js/lista-tarefas.js
@@ -0,0 +1,17 @@
+import './nav.js'
+import '../css/lista-tarefas.css'
+const inputNovaTarefa = document.querySelector ('#input-nova-tarefa')
+const btnAdicionar = document.querySelector ('#btn-adicionar')
+const listaTarefas = document.querySelector ('#lista-tarefas')
+
+btnAdicionar.addEventListener('click', function (){
+    let descricaoTarefa = inputNovaTarefa.value 
+    const li = document.createElement ('li')
+    li.className = 'tarefa'
+    li.innerHTML = `
+    <div> ${descricaoTarefa} </div>
+    <button class = "btn-excluir"> Excluir </button>
+    `
+
+    listaTarefas.appendChild(li)
+})
